87f0da341cdb3c2dded949dd2963c9259a009cc6
4ndroid3 <andijaimo@gmail.com>
2021-05-02 20:26:03 -0300

    depuracion de los modelos, quedan funcionando ok

--- peliculas_series/app/models.py
+++ peliculas_series/app/models.py
@@ -3,6 +3,80 @@
 # Django Imports
 from django.db import models
 
+# Project Imports
+from personas.models import Persona, Casting
+
+class Serie(models.Model):
+    """ Modelo que maneja el ingreso de series a la DB 
+    
+    Campos:
+    - temporada_nro: integer
+    - temporada_duracion: duration
+    - cant_cap: integer
+    """
+    temporada_nro = models.PositiveIntegerField(
+        blank = True,
+        help_text = 'Numero de la temporada',
+        verbose_name = 'Temporada Numero',
+    )
+    temporada_duracion = models.DurationField(
+        blank = True,
+        verbose_name = 'Duración',
+        help_text = 'Tiempo de duracion de la temporada completa',
+    )
+    cant_cap = models.PositiveIntegerField(
+        blank = True,
+        help_text = 'Cantidad de capitulos de la temporada',
+        verbose_name = 'Cantidad de capitulos',
+    )
+
+    class Meta:
+        verbose_name = 'Serie'
+        verbose_name_plural = 'Series'
+
+class Pelicula(models.Model):
+    """ Modelo que maneja el ingreso de peliculas a la DB 
+    
+    Campos:
+    - duracion: time
+    - director: FK
+    """
+    director = models.ForeignKey(
+        Persona,
+        on_delete = models.CASCADE,
+        blank = True,
+        null = True,
+        help_text = 'Director de la Pelicula',
+        verbose_name = 'Director',
+    )
+    duracion = models.DurationField(
+        blank = True,
+        verbose_name = 'Duración',
+        help_text = 'Tiempo de duración de la pelicula',
+    )
+    
+    class Meta:
+        verbose_name = 'Pelicula'
+        verbose_name_plural = 'Peliculas'
+
+class Tipo(models.Model):
+    """ Clase intermedia para manejar si es pelicula o serie """
+    id_serie = models.OneToOneField(
+        Serie,
+        on_delete = models.CASCADE,
+        blank = True,
+        null = True,
+    )
+    id_pelicula = models.OneToOneField(
+        Pelicula,
+        on_delete = models.CASCADE,
+        blank = True,
+        null = True,
+    )
+
+    def __str__(self):
+        return str(self.id_pelicula, self.id_serie)
+
 class Pelicula_Serie(models.Model):
     """
     Modelo de DB para la tabla de peliculas y series
@@ -24,13 +98,12 @@ class Pelicula_Serie(models.Model):
     )
 
     pelicula_serie = models.ForeignKey(
-        'app.models.Tipo',
+        Tipo,
         on_delete = models.CASCADE,
         help_text = 'Es una pelicula o una serie?',
         verbose_name = 'Pelicula / Serie', 
     )
     año = models.PositiveIntegerField(
-        max_length = 4,
         blank = True,
         null=True,
         help_text = 'Año en que se publico el libro.',
@@ -52,7 +125,7 @@ class Pelicula_Serie(models.Model):
         help_text = 'Genero de la pelicula o serie',
     )
     casting = models.OneToOneField(
-        'personas.models.Casting',
+        Casting,
         on_delete = models.CASCADE,
         help_text = 'Casting de la pelicula o serie',
         verbose_name = 'Casting',
@@ -71,76 +144,3 @@ class Pelicula_Serie(models.Model):
     class Meta:
         verbose_name = 'Pelicula o Serie'
         verbose_name_plural = 'Peliculas o Series'
-
-class Tipo(models.Model):
-    """ Clase intermedia para manejar si es pelicula o serie """
-    id_serie = models.OneToOneField(
-        'app.models.Serie',
-        on_delete = models.CASCADE,
-        blank = True,
-        null = True,
-    )
-    id_pelicula = models.OneToOneField(
-        'app.models.Pelicula',
-        on_delete = models.CASCADE,
-        blank = True,
-        null = True,
-    )
-
-    def __str__(self):
-        return str(self.id_pelicula, self.id_serie)
-
-class Serie(models.Model):
-    """ Modelo que maneja el ingreso de series a la DB 
-    
-    Campos:
-    - temporada_nro: integer
-    - temporada_duracion: duration
-    - cant_cap: integer
-    """
-    temporada_nro = models.PositiveIntegerField(
-        max_digits = 2,
-        blank = True,
-        help_text = 'Numero de la temporada',
-        verbose_name = 'Temporada Numero',
-    )
-    temporada_duracion = models.DurationField(
-        blank = True,
-        verbose_name = 'Duración',
-        help_text = 'Tiempo de duracion de la temporada completa',
-    )
-    cant_cap = models.PositiveIntegerField(
-        max_digits = 2,
-        blank = True,
-        help_text = 'Cantidad de capitulos de la temporada',
-        verbose_name = 'Cantidad de capitulos',
-    )
-
-    class Meta:
-        verbose_name = 'Serie'
-        verbose_name_plural = 'Series'
-
-class Pelicula(models.Model):
-    """ Modelo que maneja el ingreso de peliculas a la DB 
-    
-    Campos:
-    - duracion: time
-    - director: FK
-    """
-    director = models.ForeignKey(
-        'personas.models.Persona',
-        on_delete = models.CASCADE,
-        blank = True,
-        null = True,
-        help_text = 'Director de la Pelicula',
-        verbose_name = 'Director',
-    )
-    duracion = models.DurationField(
-        blank = True,
-        verbose_name = 'Duración',
-        help_text = 'Tiempo de duración de la pelicula',
-    )
-    
-    class Meta:
-        verbose_name = 'Pelicula'
-        verbose_name_plural = 'Peliculas'
